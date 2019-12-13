667a6d1a6a11ab0908f70b52fb287bf63d9eab16
Tenzin Wangchuk <tenzinwangchuk95@gmail.com>
2019-12-13 04:35:26 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,7 +12,7 @@ def happy_birthday(birthday_kids)
 end
 
 
-def age_appropreiate_birthday(birthday_kids)
+def age_appropriate_birthday(birthday_kids)
   # add your code snippet here!
   birthday_kids.each do |kids_name, age|
     if age <= 12
